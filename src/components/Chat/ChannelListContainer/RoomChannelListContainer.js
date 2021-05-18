81d7f8b2aabb75a852b0ef77835b4e5fe0ceae7b
Román Gutierrez <roman.gutierrez@gmail.com>
2021-05-18 12:44:28 -0300

    A2A widget improvements

--- src/components/Chat/ChannelListContainer/RoomChannelListContainer.js
+++ src/components/Chat/ChannelListContainer/RoomChannelListContainer.js
@@ -123,7 +123,7 @@ const RoomChannelListContainer = ({
               <span>New Custom Room</span>
             </button>
           </div>
-          <div className='has-text-centered mt-2'>
+          {/* <div className='has-text-centered mt-2'>
             <button
               className='button is-primary is-large is-fullwidth'
               onClick={async () => {
@@ -135,7 +135,7 @@ const RoomChannelListContainer = ({
             >
               Remove all rooms
             </button>
-          </div>
+          </div> */}
         </div>
       )}
       {showRoomsManager && (
